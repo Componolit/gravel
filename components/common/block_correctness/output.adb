51c0e99ef3ba78847ca8d4fc6c2260840a3b149d
Johannes Kliemann <kliemann@componolit.com>
2019-05-07 18:09:27 +0200

    prove correctness test output

--- components/common/block_correctness/output.adb
+++ components/common/block_correctness/output.adb
@@ -1,6 +1,4 @@
 
-with Cai.Log.Client;
-
 package body Output with
    SPARK_Mode
 is
@@ -15,11 +13,27 @@ is
                     C : Cai.Timer.Time;
                     P : Long_Integer) return Duration
    is
+      T : Cai.Timer.Time;
    begin
       if P < 1 or P > 999 then
-         return Duration (0);
+         return 0.0;
       end if;
-      return Duration (((C - S) / Integer (P)) * Integer (1000 - P));
+      if
+         ((S > 0.0 and then Cai.Timer.Time'First + S <= C)
+          or (S < 0.0 and then Cai.Timer.Time'Last + S >= C))
+      then
+         T := ((C - S) / Integer (P));
+         if
+            (T > 0.0 and then Cai.Timer.Time'Last / Integer (1000 - P) > T)
+            or (T < 0.0 and then Cai.Timer.Time'Last / Integer (1000 - P) < T)
+         then
+            return Duration (T * Integer (1000 - P));
+         else
+            return 0.0;
+         end if;
+      else
+         return 0.0;
+      end if;
    end Remain;
 
    function Byte_Image (Bytes : Long_Integer) return String
@@ -43,22 +57,55 @@ is
                        Last   : in out Cai.Timer.Time;
                        Log    : in out Cai.Log.Client_Session)
    is
-      P : Long_Integer := -1;
+      function Safe_Multiply (L, R : Long_Integer) return Long_Integer with
+        Pre => L >= 0 and R >= 0
+      is
+      begin
+         if L = 0 or R = 0 then
+            return 0;
+         end if;
+         if Long_Integer'Last / R > L then
+            return R * L;
+         else
+            return Long_Integer'Last;
+         end if;
+      end Safe_Multiply;
+      P : Long_Integer;
    begin
+      if
+         (Last > 0.0 and then Cai.Timer.Time'First + Last > Now)
+         or (Last < 0.0 and then Cai.Timer.Time'Last + Last < Now)
+      then
+         Last := Now;
+      end if;
       if Duration (Now - Last) > Duration (2) then
          Last     := Now;
-         P := Done / (Todo / 1000);
-         Cai.Log.Client.Info (Log, Prefix & "... ("
-                                   & Cai.Log.Image (P / 10)
-                                   & "."
-                                   & Cai.Log.Image (P rem 10)
-                                   & "%, " & Byte_Image (Done * Size)
-                                   & " / " & Byte_Image (Todo * Size)
-                                   & ")");
-         Cai.Log.Client.Info (Log, "Elapsed: "
-                                   & Cai.Log.Image (Duration (Now - Start))
-                                   & " Remaining: "
-                                   & Cai.Log.Image (Remain (Start, Now, P)));
+         P := (if Todo > 999 then Done / (Todo / 1000) else 0);
+         declare
+            S : String := Prefix & "... ("
+                                 & Cai.Log.Image (P / 10)
+                                 & "."
+                                 & Cai.Log.Image (P rem 10)
+                                 & "%, " & Byte_Image (Safe_Multiply (Done, Size))
+                                 & " / " & Byte_Image (Safe_Multiply (Todo, Size))
+                                 & ")";
+         begin
+            if S'Length > Cai.Log.Client.Maximum_Message_Length (Log) then
+               Cai.Log.Client.Info (Log, S (S'First  .. S'First + Cai.Log.Client.Maximum_Message_Length (Log) - 2));
+               Cai.Log.Client.Info (Log, S (S'First + Cai.Log.Client.Maximum_Message_Length (Log) - 1 .. S'Last));
+            else
+               Cai.Log.Client.Info (Log, S);
+            end if;
+         end;
+         if
+            (Start > 0.0 and then Cai.Timer.Time'First + Start <= Now)
+            or (Start < 0.0 and then Cai.Timer.Time'Last + Start >= Now)
+         then
+            Cai.Log.Client.Info (Log, "Elapsed: "
+                                      & Cai.Log.Image (Duration (Now - Start))
+                                      & " Remaining: "
+                                      & Cai.Log.Image (Remain (Start, Now, P)));
+         end if;
       end if;
    end Progress;
 
--- components/common/block_correctness/output.ads
+++ components/common/block_correctness/output.ads
@@ -1,10 +1,13 @@
 
 with Cai.Log;
+with Cai.Log.Client;
 with Cai.Timer;
 
 package Output with
    SPARK_Mode
 is
+
+   use type Cai.Timer.Time;
 
    procedure Progress (Prefix :        String;
                        Done   :        Long_Integer;
@@ -13,8 +16,16 @@ is
                        Start  :        Cai.Timer.Time;
                        Now    :        Cai.Timer.Time;
                        Last   : in out Cai.Timer.Time;
-                       Log    : in out Cai.Log.Client_Session);
+                       Log    : in out Cai.Log.Client_Session) with
+     Pre  => Cai.Log.Client.Initialized (Log)
+             and Prefix'Length <= 50
+             and Prefix'First = 1
+             and Done >= 0
+             and Todo >= 0
+             and Size >= 0,
+     Post => Cai.Log.Client.Initialized (Log);
 
-   function Byte_Image (Bytes : Long_Integer) return String;
+   function Byte_Image (Bytes : Long_Integer) return String with
+     Post => Byte_Image'Result'Length < 25;
 
 end Output;
