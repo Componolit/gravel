0bf2cf674f30d0f424b15bc4c950f2c3eaff80b2
Johannes Kliemann <kliemann@componolit.com>
2019-10-23 14:03:53 +0200

    block throttle timed acknowledgement

--- components/common/block_throttle/component.ads
+++ components/common/block_throttle/component.ads
@@ -18,7 +18,7 @@ package Component is
    type Byte is mod 2 ** 8;
    subtype Unsigned_Long is Long_Integer range 0 .. Long_Integer'Last;
    type Buffer is array (Unsigned_Long range <>) of Byte;
-   type Request_Index is mod 2 ** 6;
+   type Request_Index is mod 2 ** 8;
 
    package Block is new Gns.Block (Byte, Unsigned_Long, Buffer, Integer, Request_Index);
 
