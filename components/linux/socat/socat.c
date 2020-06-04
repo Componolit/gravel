55b79e6674646746dcf3c302420e7ee4ae791167
Johannes Kliemann <kliemann@componolit.com>
2020-06-04 12:01:08 +0200

    Enable socat to serve an arbitrary number of clients
    ref #10

--- components/linux/socat/socat.c
+++ components/linux/socat/socat.c
@@ -2,19 +2,17 @@
 #include <err.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-void socat_fork(int *pid)
-{
-    *pid = fork();
-    if(*pid < 0){
-        warn("%s: fork()", __func__);
-    }
-}
+#include <sys/wait.h>
 
 void socat_exec(const char *dest, const char *src)
 {
-    warn("%s, %s", dest, src);
     if(execlp("socat", "socat", dest, src, 0) < 0){
         warn("%s: execlp(socat, socat, %s, %s, 0)", __func__, dest, src);
     }
+}
+
+void socat_check_pid(int pid, int *status)
+{
+    usleep(10000); //give socat 10ms to connect
+    *status = waitpid(pid, 0, WNOHANG);
 }
